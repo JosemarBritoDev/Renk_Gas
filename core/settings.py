ef94bb24589becec575f40c6efb6cce553ecb3a1
Josemar Brito <estudos.josemarbrito@gmail.com>
2026-09-03 22:22:26 -0300

    refactor: remove forcinha manual de SSL no DATABASES

--- core/settings.py
+++ core/settings.py
@@ -92,12 +92,6 @@ DATABASES = {
     )
 }
 
-# Configura o SSL diretamente para o driver PostgreSQL do Render
-if os.getenv('DATABASE_URL'):
-    DATABASES['default']['OPTIONS'] = {
-        'sslmode': 'require',
-    }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/6.1/ref/settings/#auth-password-validators
