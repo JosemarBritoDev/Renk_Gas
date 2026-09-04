a65c148e204abc60a17e988409d0b7dde11fe151
Josemar Brito <estudos.josemarbrito@gmail.com>
2026-09-03 22:11:19 -0300

    fix(deploy): reativa ssl_require para conexao com hostname completo

--- core/settings.py
+++ core/settings.py
@@ -89,6 +89,7 @@ DATABASES = {
         default=os.getenv('DATABASE_URL', f'sqlite:///{BASE_DIR / "db.sqlite3"}'),
         conn_max_age=600,
         conn_health_checks=True,
+        ssl_require=True if os.getenv('DATABASE_URL') else False,
     )
 }
 
